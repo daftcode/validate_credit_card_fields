3c39610960eb97ea07aecaa6fdaafaa13125826d
Paryk Pastewski <patryk.pastewski@daftcode.pl>
2015-05-25 12:07:24 +0200

    removed railtie unused module extension

--- lib/daft_cc_validator/railtie.rb
+++
@@ -1,9 +0,0 @@
-module DaftCcValidator
-  class Railtie < Rails::Railtie
-    # initializer 'daft_cc_validator.initialize' do
-    #   ActiveSupport.on_load(:active_model) do
-    #     ActiveModel::Validations.send :include, DaftCcValidator
-    #   end
-    # end
-  end
-end
