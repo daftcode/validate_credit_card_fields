15d8ef8bc2d0e8da86521d30c0ef2f85a3d2973c
Paryk Pastewski <patryk.pastewski@daftcode.pl>
2015-05-25 18:39:33 +0200

    removed duplicated require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,9 @@ require 'bundler/setup'
 Bundler.setup
 
 require 'active_model'
-require 'active_model'
 require 'byebug'
 require 'daft_cc_validator'
 require 'dummy/user'
-
-# require 'daft_cc_validator_spec' # and any other gems you need
 
 RSpec.configure do |config|
   # some (optional) config here
